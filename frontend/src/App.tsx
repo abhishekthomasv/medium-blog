ca4143886453d47fdac61f8c178a0defba8c9c1f
abhishekthomasv <abhishek.180thomas@gmail.com>
2024-06-25 22:29:56 +0530

    set default route to sign in page.

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -14,6 +14,7 @@ function App() {
     <>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Signin />} />
           <Route path="/blogs" element={<Blogs />} />
           <Route path="/signup" element={<Signup />} />
           <Route path="/signin" element={<Signin />} />
